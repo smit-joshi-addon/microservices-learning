0cc7ac4fae77dab5919009fabcff7072a47668b9
smit joshi <intern-j05@addontechnologies.net>
2024-07-23 15:49:37 +0530

    first commit

---
+++ api-gateway/src/main/java/com/smit/eureka/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.smit.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ config-server-addon/src/test/java/com/smit/config/ConfigServerAddonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smit.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerAddonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
